7195184290e5afd93f5ff4a961922bd2faedfc05
BryanL920 <115898375+BryanL920@users.noreply.github.com>
2022-12-16 19:27:14 -0600

    Revert "Bout to break it i think"
    This reverts commit 89d4515b10b3581171062c653287742c54377550.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,12 @@ import theme from "../styles/theme";
 import { ThemeProvider } from "@material-ui/core/styles";
 import Topbar from "../components/Topbar";
 import Sidebar from "../components/Sidebar";
-import Content from "../components/Content";
 
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <Topbar />
       <Sidebar />
-      <Content />
     </ThemeProvider>
   );
 }
